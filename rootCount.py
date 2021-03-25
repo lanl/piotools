dab6def970607b2a4d5ab09b5923cb01c4178a1c
Sriram Swaminarayan <sriram@lanl.gov>
2021-03-25 12:52:22 -0600

    made a subroutine inside rootCount.py that can be used elsewhere

--- rootCount.py
+++ rootCount.py
@@ -1,28 +1,30 @@
-from pio import pio
+def getRootSize(p):
+    """
+    Returns the root mesh size of pio file p
+    """
+    l1 = p.readArray(b"cell_level_0")
+    l0 = l1[0]
+    for i in range(p.numcell-1,0,-1):
+        if l1[i] == l0:
+            break
 
-import sys
-
-def readOneEntry(p, name, j):
-    return p.readArrayRange(name,j,1)[0]
+    c = [0]*p.ndim
+    d = [0]*p.ndim
+    e = [0]*p.ndim
+    r = [0]*p.ndim
+    offsets = [1, 2, 4]
+    for dim in range(p.ndim):
+        name = b"cell_center_%1d"%(dim+1)
+        c[dim] = p.readArrayRange(name, 0, 1)[0]
+        d[dim] = p.readArrayRange(name, offsets[dim], offsets[dim]+1)[0]
+        e[dim] = p.readArrayRange(name, i, i+1)[0]
+        r[dim] = 1+round((e[dim] - c[dim])/(d[dim] - c[dim]))
     
-p = pio(sys.argv[1])
+    return r
 
-l1 = p.readArray(b"cell_level_0")
-l0 = l1[0]
-for i in range(p.numcell-1,0,-1):
-    if l1[i] == l0:
-        break
+if __name__ == "__main__":
+    import sys
+    from pio import pio
+    p = pio(sys.argv[1])
+    print(f"Root mesh size = {getRootSize(p)}")
 
-c = [0]*p.ndim
-d = [0]*p.ndim
-e = [0]*p.ndim
-r = [0]*p.ndim
-offsets = [1, 2, 4]
-for dim in range(p.ndim):
-    name = b"cell_center_%1d"%(dim+1)
-    c[dim] = readOneEntry(p, name, 0)
-    d[dim] = readOneEntry(p, name, offsets[dim])
-    e[dim] = readOneEntry(p, name, i)
-    r[dim] = 1+round((e[dim] - c[dim])/(d[dim] - c[dim]))
-
-print(f"Root mesh size = {r}")
