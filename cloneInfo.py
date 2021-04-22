2a9212cb86947e1eeb0c6ae0d4ddcfb97685967c
Sriram Swaminarayan <sriram@lanl.gov>
2021-04-22 17:05:56 -0600

    formatting changes

--- cloneInfo.py
+++ cloneInfo.py
@@ -27,6 +27,8 @@ Date: April 22, 2021
 Version: 1.0
 
 """
+
+
 def CloneInfo(f, verbose=False):
     import numpy as np
     from pio import pio
@@ -43,7 +45,7 @@ def CloneInfo(f, verbose=False):
         gns = [p.numcell]
 
     if verbose:
-        print(f'nProcs={len(gns)}')
+        print(f"nProcs={len(gns)}")
     # generate procID
     if verbose:
         print("Generating procid")
@@ -52,45 +54,49 @@ def CloneInfo(f, verbose=False):
     last = int(0)
     for x in gns:
         x = int(x)
-        procid.extend([id]*x)
-        print(f'Proc {id:>6}: {last+1:>8} - {last+x:<8}')
+        procid.extend([id] * x)
+        print(f"Proc {id:>6}: {last+1:>8} - {last+x:<8}")
         last = len(procid)
         id += 1
-    
+
     numcell = p.numcell
-    nbrs = np.empty([numcell, 2 * ndim],dtype='int')
+    nbrs = np.empty([numcell, 2 * ndim], dtype="int")
     for i in range(ndim):
         if verbose:
-            print("Generating neighbors in direction",i)
+            print("Generating neighbors in direction", i)
         idx = 2 * i + 1
-        nl = p.readArray(b"cell_index_%1d"%(idx))
-        nh = p.readArray(b"cell_index_%1d"%(idx+1))
+        nl = p.readArray(b"cell_index_%1d" % (idx))
+        nh = p.readArray(b"cell_index_%1d" % (idx + 1))
         for k in range(numcell):
-            nbrs[k,idx-1] = int(nl[k]-1)
-            nbrs[k,idx] = int(nh[k]-1)
+            nbrs[k, idx - 1] = int(nl[k] - 1)
+            nbrs[k, idx] = int(nh[k] - 1)
 
     if verbose:
         print("Neighbor generation done.")
 
     nClones = 0
     nMother = 0
-    print('numcell=',p.numcell)
+    print("numcell=", p.numcell)
     for i in range(p.numcell):
         myProc = procid[i]
         if dtr[i] > 0:
             nMother += 1
             continue
         for x in nbrs[i]:
-            if ( x < 1 or
-                 x == i or
-                 procid[x] != myProc):
+            if x < 1 or x == i or procid[x] != myProc:
                 nClones += 1
 
-    return {'numcell':numcell, 'nClones':nClones, 'nMother':nMother, 'nTop':numcell-nMother}
-    
+    return {
+        "numcell": numcell,
+        "nClones": nClones,
+        "nMother": nMother,
+        "nTop": numcell - nMother,
+    }
+
 
 if __name__ == "__main__":
     import sys
+
     files = sys.argv[1:]
     for f in files:
-        print(CloneInfo(f,1))
+        print(CloneInfo(f, 1))
